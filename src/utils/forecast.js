0190800bedd8128c4292335058e7f5787b9daebc
Mohamed Mohiey <mohhiey@gmail.com>
2021-05-31 15:10:04 +0200

    second commit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,5 @@
 const request = require("request");
-const { openweatherKey } = require("../../key") || process.env.openweatherKey;
+const openweatherKey = process.env.openweatherKey;
 
 module.exports = forecast = (longitude, latitude, callback) => {
   const openWeatherURL = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${openweatherKey}`;
